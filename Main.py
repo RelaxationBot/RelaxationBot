ef58115e443400a90b3405f32c69c72b32e17383
RelaxationBot <46949884+RelaxationBot@users.noreply.github.com>
2019-01-22 20:15:38 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ client = commands.Bot(command_prefix = '!')
 @client.event
 async def on_member_join(member):
     print('Recognised that a member called ' + member.name + ' joined')
-    await client.send_message(member, 'Welcome to **Christian's Relaxation Server!" Here you can listen to music, chat with friends, make new friends, and so much more!')
+    await client.send_message(member, "Welcome to **Christian's Relaxation Server! Here you can listen to music, chat with friends, make new friends, and so much more!")
     print('Sent message to ' + member.name)
 
 @client.event
@@ -23,15 +23,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == '-relax':
-        await client.send_message(message.channel,'Sit back, take three deep breaths. In.......count to five....out....count to five. Do that ten times, lay back, and take a nap!')
+        await client.send_message(message.channel, 'Sit back, take three deep breaths. In.......count to five....out....count to five. Do that ten times, lay back, and take a nap!')
     if message.content == '-asmr':
-        await client.send_message(message.channel,'https://www.youtube.com/channel/UClqNSqnWeOOUVkzcJFj4rBw')
+        await client.send_message(message.channel, 'https://www.youtube.com/channel/UClqNSqnWeOOUVkzcJFj4rBw')
     if message.content == '-help':
-        await client.send_message(message.channel,'@ChristianBeneventi#0001 Contact him or another staff member!')
+        await client.send_message(message.channel, '@ChristianBeneventi#0001 Contact him or another staff member!')
     if message.content == '-nou':
-        await client.send_message(message.channel,'-yesu')
+        await client.send_message(message.channel, '-yesu')
     if message.content == '-whoami':
-        await client.send_message(message.channel,'A person. Are you dumb or somethin'?')
+        await client.send_message(message.channel, "A person. Are you dumb or somethin'?")
     if message.content == '-no':
         await client.send_message(message.channel,'yes')
     if message.content == '-yes':
@@ -51,4 +51,3 @@ async def on_message(message):
     await client.process_commands(message)
 
 client.run(os.environ['BOT_TOKEN'])
-                                  
