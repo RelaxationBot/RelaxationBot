bbf1eca6fc75cf7821f1a9b1115ae34524e9e92e
RelaxationBot <46949884+RelaxationBot@users.noreply.github.com>
2019-01-22 20:05:39 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,11 +6,8 @@ import time
 import random
 from discord import Game
 
-
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
-Clientdiscord = discord.Client()
-
 
 @client.event
 async def on_member_join(member):
@@ -20,5 +17,38 @@ async def on_member_join(member):
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='test'))
+    await client.change_presence(game=discord.Game(name='test'))
     print('Ready, Freddy')
+
+@client.event
+async def on_message(message):
+    if message.content == '-relax':
+        await client.send_message(message.channel,'Sit back, take three deep breaths. In.......count to five....out....count to five. Do that ten times, lay back, and take a nap!')
+    if message.content == '-asmr':
+        await client.send_message(message.channel,'https://www.youtube.com/channel/UClqNSqnWeOOUVkzcJFj4rBw')
+    if message.content == '-help':
+        await client.send_message(message.channel,'@ChristianBeneventi#0001 Contact him or another staff member!')
+    if message.content == '-nou':
+        await client.send_message(message.channel,'-yesu')
+    if message.content == '-whoami':
+        await client.send_message(message.channel,'A person. Are you dumb or somethin'?')
+    if message.content == '-no':
+        await client.send_message(message.channel,'yes')
+    if message.content == '-yes':
+        await client.send_message(message.channel,'no')
+    if message.content == '-stop':
+        await client.send_message(message.channel,'go')
+    if message.content == '-ping':
+        await client.send_message(message.channel,'pong')
+    if message.content == '-die':
+        await client.send_message(message.channel,'why')
+    if message.content == '-commands':
+        await client.send_message(message.channel,'```-commands, -relax, -asmr, -no, -yes, -nou, -help, -ping, -stop, -die, -troomp, -duck, -saloot, -hungry, -haylp, -ping, ')
+    if message.content == '-troomp':
+        em = discord.Embed(description='make merica grate egein')
+        em.set_image(url='https://i.pinimg.com/736x/d6/3b/3e/d63b3e3087092152d34672504b4f3b7c.jpg')
+        await client.send_message(message.channel, embed=em)
+    await client.process_commands(message)
+
+client.run(os.environ['BOT_TOKEN'])
+                                  
